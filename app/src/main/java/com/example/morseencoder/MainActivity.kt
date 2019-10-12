6a334c0d7c565250a04772d94d65e416329d72ae
HC-Strong <stronghannahc@gmail.com>
2019-10-11 22:29:08 -0700

    added dependencies for data binding and reformatted fragment xml to be ready for it

--- app/src/main/java/com/example/morseencoder/MainActivity.kt
+++ app/src/main/java/com/example/morseencoder/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.morseencoder
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.morseencoder.screens.EncoderFragment
+import com.example.morseencoder.screens.SenderFragment
+import com.example.morseencoder.screens.SentFragment
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    EncoderFragment.OnFragmentInteractionListener,
+    SenderFragment.OnFragmentInteractionListener,
+    SentFragment.OnFragmentInteractionListener {
+
+
+    override fun onFragmentInteraction(uri: Uri) {
+        Timber.i("A fragment interacted with the main activity. Not doing anything with that info as of now, but it's good to know.")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
